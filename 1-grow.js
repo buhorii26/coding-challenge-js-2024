8e7ae317023d3e1fc6c19af77e9dd8ed197c0e45
Buhori <buhorics@gmail.com>
2024-02-11 14:32:19 +0900

    Day 1/366

---
+++ 1-grow.js
@@ -0,0 +1,31 @@
+/*
+1. Question : 
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+
+/* 
+Jawaban cara pertama
+-----------------------
+function grow(x) {
+  let result = x[0];
+  for (let i = 1; i < x.length; i++) {
+    result *= x[i];
+  }
+  return result;
+}
+console.log(grow([2, 2, 2, 2, 2, 2]));
+*/
+/*
+Jawaban cara kedua menggunakan fungsi reduce
+---------
+function grow(x) {
+  const result = x.reduce((accumulator, current) => accumulator * current, 1);
+  return result;
+}
+--------
+ */
+
+//dibawah ini arrow function
+const grow = (x) => x.reduce((accumulator, current) => accumulator * current, 1);
+console.log(grow([2, 2, 2, 2, 2, 2]));
